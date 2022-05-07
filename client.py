faed7ccb8e2f48e8907e370e99eb8fc825e83bda
Youssef Nawar <youssef.nawar10@yahoo.com>
2022-05-07 14:55:43 +0200

    awl 7aga

--- client.py
+++ client.py
@@ -2,12 +2,42 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+# HOST = "127.0.0.1"  # The server's hostname or IP address
+# PORT = 65432  # The port used by the server
+# dir = "/Users/youssefnawar/PycharmProjects/NetworksProject/"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+def parse_file(file_name):
+    f = open(f"{file_name}", mode = "r")
+    commands = f.read().split("\n")
+    f.close()
+    return commands
 
-print(f"Received {data!r}")
+def parse(cmd):
+    x = cmd.split()
+    method = x[0]
+    file_name = x[1]
+    host = x[2]
+    if len(x) > 3:
+        port = x[3]
+        return method,file_name,host,port
+    return method,file_name,host,80
+
+commands = parse_file("commands.txt")
+for i in commands:
+    method , file_name, HOST, PORT = parse(i)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        request = f"{method} {file_name} HTTP/1.0\nHOST :{HOST} {PORT}" 
+        s.sendall(bytes(request,"utf-8"))
+        if method is "GET":
+            data = s.recv(1024)
+            print(f"Received {data!r}")
+        elif method is "POST":
+            try:
+                f = open(f"{file_name}",mode ="r")
+                file = f.read()
+                f.close()
+                s.sendall(bytes(file,"utf-8"))
+            except IOError:
+                print("FILE NOT FOUND")
+        s.close()
--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 # echo-server.py
 
 import socket
+from _thread import *
+import threading
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+def parse_command(command):
+    x = command.split()
+    method = x[0]
+    file_name = x[1]
+    return method , file_name
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -13,6 +20,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+            request = decode
             if not data:
                 break
             conn.sendall(data)
